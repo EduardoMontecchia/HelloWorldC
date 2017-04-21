40edc5d261aed6e4baccd576137e9f6fc52a0637
EduardoMontecchia <montecchia2@hotmail.com>
2017-04-21 13:00:39 -0300

    Add files via upload

---
+++ new  1.hs
@@ -0,0 +1,8 @@
+maximo x y = max x.max y
+
+
+redondeo = fromIntegral.ceiling
+
+cantPizas cantP = redondeo ( (*(3/8))cantP)
+
+cuantoPagaCU precio comensales = ((precio*) (cantPizas comensales)/comensales)
